cf1ee84285682a8bc264505291a7938afcd8ebd6
haawa799 <haawaplus@gmail.com>
2014-10-12 00:01:42 +0200

    Project renamed

---
+++ CAMonymous/ToolbarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ToolbarViewController.swift
+//  CAMonymous
+//
+//  Created by Andrew K. on 10/11/14.
+//  Copyright (c) 2014 CAMonymous_team. All rights reserved.
+//
+
+import UIKit
+
+class ToolbarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
